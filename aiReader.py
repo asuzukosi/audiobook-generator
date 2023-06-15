cb6de531a1c607ef7dfd3caad1e7fd42f8eeabbd
asuzukosi <keloasuzu@yahoo.com>
2023-06-15 03:44:35 +0100

    feat: setup application to use gpu instead of cpu

--- aiReader.py
+++ aiReader.py
@@ -122,7 +122,7 @@ def speechGeneratorMicrosoft(text, fileName):
         
         input_sequence = processor(text=data, return_tensors="pt")
         # load xvector containing speaker's voice characteristics from a dataset
-        embeddings_dataset = load_dataset("Matthijs/cmu-arctic-xvectors", split="validation").to(device)
+        embeddings_dataset = load_dataset("Matthijs/cmu-arctic-xvectors", split="validation")
         speaker_embeddings = torch.tensor(embeddings_dataset[7306]["xvector"]).unsqueeze(0).to(device)
         speech = model.generate_speech(input_sequence["input_ids"].to(device), speaker_embeddings, vocoder=vocoder)
         output = np.concatenate([output, speech.numpy()], axis=0)
